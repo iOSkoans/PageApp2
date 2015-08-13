5d2d07b65ec18afba21ad1f09079949123521264
Calvin Cheng <linchuan.cheng@gmail.com>
2015-08-13 17:02:36 +0800

    Initial Commit

---
+++ PageApp2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PageApp2
+//
+//  Created by Calvin Cheng on 13/8/15.
+//  Copyright © 2015 Hello HQ Pte. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
